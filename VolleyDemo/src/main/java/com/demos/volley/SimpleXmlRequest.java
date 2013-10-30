395636cc740e78643856ee0d3ce2cbb54af3ae74
darja <ifndef@yandex.ru>
2013-10-30 18:26:41 +0400

    SimpleXmlRequest draft

---
+++ VolleyDemo/src/main/java/com/demos/volley/SimpleXmlRequest.java
@@ -0,0 +1,47 @@
+package com.demos.volley;
+
+import com.android.volley.NetworkResponse;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.HttpHeaderParser;
+import org.simpleframework.xml.Serializer;
+import org.simpleframework.xml.core.Persister;
+
+import java.io.UnsupportedEncodingException;
+import java.util.regex.Matcher;
+
+public class SimpleXmlRequest<T> extends ExtendedRequest<T> {
+    private final Class<T> mResponseType;
+
+    private Matcher mMatcher;
+
+
+    public SimpleXmlRequest(Class<T> responseType, int method, String url,
+                        Response.Listener<T> listener, Response.ErrorListener errorListener) {
+        super(method, url, listener, errorListener);
+        mResponseType = responseType;
+    }
+
+    public void setMatcher(Matcher matcher) {
+        mMatcher = matcher;
+    }
+
+    @Override
+    protected Response<T> parseNetworkResponse(NetworkResponse response) {
+        String responseString;
+        try {
+            responseString = getResponseString(response);
+        } catch (UnsupportedEncodingException e) {
+            return Response.error(new VolleyError(e));
+        }
+
+        Serializer serializer = mMatcher == null ? new Persister() : new Persister(mMatcher);
+        try {
+            T result = serializer.read(mResponseType, responseString);
+            return Response.success(result, HttpHeaderParser.parseCacheHeaders(response));
+
+        } catch (Exception e) {
+            return Response.error(new VolleyError(e));
+        }
+    }
+}
